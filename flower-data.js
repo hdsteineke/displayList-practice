8ecf7d35d2ecdd05b2536933af64a23fca2732a8
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-15 11:45:29 -0700

    wrote array for flowers and hard-coded first flower in HTML to see what I wanted to render

---
+++ flower-data.js
@@ -0,0 +1,23 @@
+export const flowers = [
+    {
+        name: 'lilac',
+        latin_name: 'Syringa vulgaris',
+        color: 'purple, white',
+    
+    },
+
+    {
+        name: 'dogwood',
+        latin: 'Cornus florida',
+        color: 'white, pink'
+
+    },
+
+    {
+        name: 'magnolia',
+        latin: 'Magnolia x soulangeana',
+        color: 'white and pink'
+
+    }
+
+];
